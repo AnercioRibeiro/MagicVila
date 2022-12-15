f3b7dbdb1eaab340817345587c4a1cc6c9018627
Minotauro <minotauro.olimpo@outlook.pt>
2022-12-15 00:24:17 -0800

    Response type

---
+++ Controllers/VillaAPIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MagicVila_VilaAPI.Data;
+using MagicVila_VilaAPI.Models;
+using MagicVila_VilaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVila_VilaAPI.Controllers
+{
+    //[Route("api/[Controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+        [HttpGet]
+		public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.VillaList);
+             
+        }
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200, Type = typeof(VillaDTO))]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+
+        }
+    }
+}
+
---
+++ Data/VillaStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MagicVila_VilaAPI.Models.Dto;
+
+namespace MagicVila_VilaAPI.Data
+{
+	public class VillaStore
+	{
+        public static List<VillaDTO> VillaList = new List<VillaDTO>
+        {
+               new VillaDTO {Id=1, Name="Pool View"},
+                new VillaDTO {Id=2, Name="Beach View"},
+            
+        };
+     }
+}
+
