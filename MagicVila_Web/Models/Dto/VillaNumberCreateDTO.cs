fc70d505af29154e11240e2ea4d516099c54177d
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-01-09 14:29:07 +0100

    Add Utility project, AutoMapper to MagicVila_Web

---
+++ MagicVila_Web/Models/Dto/VillaNumberCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVila_Web.Models.Dto
+{
+	public class VillaNumberCreateDTO
+	{
+        [Required]
+        public int VillaNo { get; set; }
+        [Required]
+        public int VillaID { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+
+}
+
---
+++ MagicVila_Web/Utilities/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MagicVila_VilaAPI.Models;
+using MagicVila_Web.Models.Dto;
+
+namespace MagicVila_Web.Utilities
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
