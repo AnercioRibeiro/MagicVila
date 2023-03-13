bc379e763abe1db6a5ba88b4a6145e8901638584
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-03-13 15:52:46 +0100

    Refactoring

--- MagicVila_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MagicVila_Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
